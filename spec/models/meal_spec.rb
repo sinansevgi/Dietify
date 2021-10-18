400d120d84be7e2e30a25845e1155b870709fbeb
Sinan Sevgi <sinansevgi@users.noreply.github.com>
2021-10-18 17:22:56 +0300

    Add Tests for models

---
+++ spec/models/meal_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Meal, type: :model do
+  # Association test
+  # ensure Meal model has a one-to-many relationship with the Food model
+  it { should have_many(:foods).dependent(:destroy) }
+  # Validation tests
+  # ensure columns title and created_by are present before saving
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:created_by) }
+end
